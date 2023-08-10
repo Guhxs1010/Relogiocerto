a2168fe8efcaf29342c07c03b542e1fdde4978b5
Guhxs1010 <133364786+Guhxs1010@users.noreply.github.com>
2023-08-10 08:50:17 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,16 @@
+// Random Gera números aleatorios
+console.log(Math.random());
+console.log(Math.random()*20);
+console.log(Math.random(7 - 3+1)+3);
+
+// arrendonda intermediario 
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// Arredonda pra baixo 
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
+
+// Arredonda pra cima 
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
